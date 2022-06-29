8965c292753d6b975cb50e1f12338257f23961d6
MaddieJane <67866550+MadelineJaneWadsworth@users.noreply.github.com>
2022-06-29 10:00:29 -0400

    Added Segues

--- HavenFinalApp/ViewController.swift
+++ HavenFinalApp/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var havenLabel: UILabel!
+    @IBOutlet weak var havenLogo: UIImageView!
+    
+    
+    @IBAction func abortionInformationButton(_ sender: Any) {
+    }
+    
+    @IBAction func bodyImageInformationButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func menstruationInformationButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func domesticAbuseInformationButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
